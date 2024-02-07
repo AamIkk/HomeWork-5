ca43b61a406efa55bf97c73e48c07fa76cdfeccb
AamIkk <adylaibekov81@gmail.com>
2024-02-07 17:07:02 +0600

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const categories=document.getElementById('categories')
 const products=document.getElementById('products')
 const cartCount=document.getElementById('cartCount')
+const heroBlock=document.querySelector('.heroBlock')
+const cCart=document.querySelector('#onCart')
 
 const _shopUrl='https://fakestoreapi.com/products/'
 
@@ -8,8 +10,8 @@ async function getShop(){
     const res=await fetch(_shopUrl)
     const data=await res.json()
     console.log(data);
-    showCotegories(data);
-    renderProducts(data)
+    showCotegories(data)
+    renderProducts(data.slice(0, 4))
 }
 
 getShop()
@@ -66,7 +68,7 @@ function showOneCard(obj){
       <img src=${obj.image} class="card-img-top" alt="...">
   <div class="card-body">
       <h5 class="card-title">${obj.title}</h5>
-      <p class="card-text">${obj.description}</p>
+      <p class="card-text">${obj.category}</p>
       <h4>${obj.price}$</h4>
       <a href="#" onclick='addItemToCart(${obj.id})' class="btn btn-primary">Добавить в корзину!</a>
    </div>
@@ -76,19 +78,93 @@ function showOneCard(obj){
 let arrCart=[]
 
 async function addItemToCart(id){
-    const res=await fetch(_shopUrl+id)
-    const data=await res.json()
+    const res = await fetch(_shopUrl + id);
+    const data = await res.json();
     console.log(data);
-    arrCart.push(data)
-    const cartData=JSON.stringify(arrCart)
-    localStorage.setItem('cart', cartData )
-    getItemFromCart()
+
+    const existingItem = arrCart.find(item => item.id === data.id);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        data.quantity = 1;
+        arrCart.push(data);
+    }
+    
+    const cartData = JSON.stringify(arrCart);
+    localStorage.setItem('cart', cartData);
+    getItemFromCart(cartData);
 }
 
-function getItemFromCart(){
-    const data=JSON.parse(localStorage.getItem('cart'))
-    console.log(data, 'localStorage');
-    arrCart=data
+cCart.onclick=()=>{ 
+    products.innerHTML=''
+    heroBlock.innerHTML='' 
+    showCotegories.innerHTML=''
+    showOneCard.innerHTML=''
+    showCart(arrCart) 
+   
+  } 
+   
+  function showCart(arr){ 
+    heroBlock.innerHTML='' 
+
+    let totalSumma = 0;
+   
+    arr.forEach((item, index) => { 
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('d-flex', 'justify-content-between');
+        itemElement.dataset.index = index;
+        itemElement.innerHTML = `
+            <img src='${item.image}' style='max-width: 30%' /> 
+            <div> 
+                <h2>${item.title}</h2> 
+                <h5>${item.category}</h5> 
+            </div> 
+            <h1>${item.price}</h1> 
+            <div> 
+                <button class="plus-btn">+</button> 
+                <span>${item.quantity}</span> 
+                <button class="minus-btn">-</button> 
+            </div> 
+            <button class="remove-btn">x</button> 
+        `;
+        heroBlock.appendChild(itemElement);
+
+        totalSumma += item.price * item.quantity;
+        
+    }); 
+
+    const totalBlock = document.createElement('h4');
+    totalBlock.textContent = `Total: ${totalSumma}$`;
+    heroBlock.appendChild(totalBlock);
+
+    const plusButtons = document.querySelectorAll('.plus-btn');
+    plusButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const index = button.parentElement.parentElement.dataset.index;
+            arr[index].quantity++;
+            showCart(arr)
+        });
+    });
+
+    const minusButtons = document.querySelectorAll('.minus-btn');
+    minusButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const index = button.parentElement.parentElement.dataset.index;
+            if (arr[index].quantity > 1) {
+                arr[index].quantity--;
+            } else {
+                arr.splice(index, 1); 
+            }
+            showCart(arr);
+        });
+    });
+
+    const removeButtons = document.querySelectorAll('.remove-btn');
+    removeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const index = button.parentElement.dataset.index;
+            arr.splice(index, 1);
+            showCart(arr);
+        });
+    });
 }
-getItemFromCart()
-cartCount.innerHTML=arrCart.length
